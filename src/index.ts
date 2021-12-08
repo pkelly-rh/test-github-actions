5a3dc832f17f51d125eb55987b5b59a4f6ca4f40
Patrick Kelly <pkelly@rh.com>
2021-12-08 10:19:27 -0800

    fix30

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,6 @@ function main () {
 
   // new proposed changes
 
-  console.log("foo")
-
-
-  console.log("new console log")
-
-  console.log("new console log 2")
-
-
-  console.log("new console log 3")
   
 }
 

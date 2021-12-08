9edc17e8068a9696a5a4da3d02dbad4d28ec1bd6
Patrick Kelly <pkelly@rh.com>
2021-12-07 17:59:30 -0800

    goodtimes:

--- public/scripts/gistfile.sh
+++ public/scripts/gistfile.sh
@@ -5,10 +5,12 @@
 # Redirect output to stderr.
 exec 1>&2
 
-consoleregexp='console.log'
+consoleregexp='console.log('
+# git diff main..special-branch | grep console.log\( | wc -l
 # CHECK
-if test $(git diff --cached | grep $consoleregexp | wc -l) != 0
+if test $(git diff main..special-branch | grep $consoleregexp | wc -l) != 0
 then 
+  echo "git diff main..special-branch | grep $consoleregexp | wc -l"
   echo "FAILURE"
-  exec git diff --cached | grep -ne $consoleregexp
+  exit 69
 fi
